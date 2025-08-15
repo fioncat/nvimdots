c65f832c1d76f11534f7f7a0a14ea1d54c7641a3
fioncat <lazycat7706@gmail.com>
2025-08-15 11:32:22 +0800

    add custom settings

---
+++ lua/user/event.lua
@@ -0,0 +1,39 @@
+local definitions = {
+	bufs = {},
+
+	ft = {
+		{ "FileType", "json", "setlocal tabstop=4" },
+		{ "FileType", "json", "setlocal shiftwidth=4" },
+		{ "FileType", "json", "setlocal expandtab" },
+
+		{ "FileType", "jsonc", "setlocal tabstop=4" },
+		{ "FileType", "jsonc", "setlocal shiftwidth=4" },
+		{ "FileType", "jsonc", "setlocal expandtab" },
+
+		{ "FileType", "json", "setlocal tabstop=4" },
+		{ "FileType", "json", "setlocal shiftwidth=4" },
+		{ "FileType", "json", "setlocal expandtab" },
+
+		{ "FileType", "yaml", "setlocal tabstop=2" },
+		{ "FileType", "yaml", "setlocal shiftwidth=2" },
+		{ "FileType", "yaml", "setlocal expandtab" },
+
+		{ "FileType", "css", "setlocal tabstop=4" },
+		{ "FileType", "css", "setlocal shiftwidth=4" },
+		{ "FileType", "css", "setlocal expandtab" },
+
+		{ "FileType", "toml", "setlocal tabstop=4" },
+		{ "FileType", "toml", "setlocal shiftwidth=4" },
+		{ "FileType", "toml", "setlocal expandtab" },
+
+		{ "FileType", "sh", "setlocal tabstop=2" },
+		{ "FileType", "sh", "setlocal shiftwidth=2" },
+		{ "FileType", "sh", "setlocal expandtab" },
+
+		{ "FileType", "zsh", "setlocal tabstop=2" },
+		{ "FileType", "zsh", "setlocal shiftwidth=2" },
+		{ "FileType", "zsh", "setlocal expandtab" },
+	},
+}
+
+return definitions
---
+++ lua/user/keymap/completion.lua
@@ -0,0 +1,61 @@
+local bind = require("keymap.bind")
+local map_cr = bind.map_cr
+local map_cu = bind.map_cu
+-- local map_cmd = bind.map_cmd
+local map_callback = bind.map_callback
+
+local mappings = {}
+
+-- Place global keymaps here.
+mappings["plug_map"] = {}
+
+-- NOTE: This function is special! Keymaps defined here are ONLY effective in buffers with LSP(s) attached
+-- NOTE: Make sure to include `:with_buffer(buf)` to limit the scope of your mappings.
+---@param buf number @The effective bufnr
+mappings["lsp"] = function(buf)
+	return {
+		-- LSP
+		["n|gd"] = map_cr("Glance definitions"):with_noremap():with_silent():with_desc("lsp: jump to definitions"),
+		["n|gr"] = map_cr("Glance references"):with_noremap():with_silent():with_desc("lsp: jump to references"),
+		["n|gi"] = map_cr("Glance implementations")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: jump to implementations"),
+		["n|gt"] = map_cr("Glance lsp_type_definitions")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: jump to type definitions"),
+		["n|gh"] = map_cr("Lspsaga lsp_finder"):with_noremap():with_silent():with_desc("lsp: launch finder"),
+		["n|K"] = map_cr("Lspsaga hover_doc"):with_noremap():with_silent():with_desc("lsp: display hover document"),
+		["n|<leader>en"] = map_cr("Lspsaga diagnostic_jump_next")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: jump to next error"),
+		["n|<leader>ep"] = map_cr("Lspsaga diagnostic_jump_prev")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: jump to previous error"),
+		["n|<leader>se"] = map_cr("Telescope diagnostics")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: search diagnostics"),
+		["n|<leader>ee"] = map_cr("TroubleToggle workspace_diagnostics")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: search workspace diagnostics"),
+		["n|<leader>ca"] = map_cr("Lspsaga code_action")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: trigger code action for current line"),
+		["v|<leader>ca"] = map_cu("Lspsaga range_code_action")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: trigger code action for multiple lines"),
+		["n|<leader>cr"] = map_cr("Lspsaga rename")
+			:with_noremap()
+			:with_silent()
+			:with_desc("lsp: rename for current word"),
+	}
+end
+
+return mappings
